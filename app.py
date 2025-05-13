852e8c75aab7dad6afae23ff6b488212cc1f8917
INDRAJITSINH1101 <indrajitsinh234@gmail.com>
2025-05-13 13:11:52 -0700

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect,flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from flask_mail import Mail
@@ -54,21 +54,25 @@ class Posts(db.Model):
 def home():
     posts = Posts.query.filter_by().all()
     last = math.ceil(len(posts)/int(params['no_of_posts']))
+    
     page = request.args.get('page')
-    if (not str(page).isnumeric()):
+    if(not str(page).isnumeric()):
         page = 1
-    page = int(page)
-    posts = posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+ int(params['no_of_posts'])]
-    if page==1:
+    page= int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']): (page-1)*int(params['no_of_posts'])+ int(params['no_of_posts'])]
+    
+    if (page==1):
         prev = "#"
         next = "/?page="+ str(page+1)
-    elif page==last:
-        prev = "/?page="+ str(page-1)
+    elif(page==last):
+        prev = "/?page=" + str(page - 1)
         next = "#"
     else:
-        prev = "/?page="+ str(page-1)
-        next = "/?page="+ str(page+1)
-    
+        prev = "/?page=" + str(page - 1)
+        next = "/?page=" + str(page + 1)
+
+
+
     return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 
@@ -81,28 +85,32 @@ def post_route(post_slug):
 def about():
     return render_template('about.html', params=params)
 
+
 @app.route("/dashboard", methods=['GET', 'POST'])
 def dashboard():
-    if "user" in session and session['user']==params['admin_user']:
+
+    if ('user' in session and session['user'] == params['admin_user']):
         posts = Posts.query.all()
-        return render_template("dashboard.html", params=params, posts=posts)
+        return render_template('dashboard.html', params=params, posts = posts)
 
-    if request.method=="POST":
-        username = request.form.get("uname")
-        userpass = request.form.get("upass")
-        if username==params['admin_user'] and userpass==params['admin_password']:
+
+    if request.method=='POST':
+        username = request.form.get('uname')
+        userpass = request.form.get('upass')
+        if (username == params['admin_user'] and userpass == params['admin_password']):
             
-            session['user']=username
+            session['user'] = username
             posts = Posts.query.all()
-            return render_template("dashboard.html", params=params, posts=posts)
-    else:
-        return render_template("login.html", params=params)
+            return render_template('dashboard.html', params=params, posts = posts)
 
-@app.route("/edit/<string:sno>", methods=['GET', 'POST'])
+    return render_template('login.html', params=params)
+
+
+
+@app.route("/edit/<string:sno>", methods = ['GET', 'POST'])
 def edit(sno):
-    if "user" in session and session['user'] == params['admin_user']:
-        if request.method == "POST":
-           
+    if ('user' in session and session['user'] == params['admin_user']):
+        if request.method == 'POST':
             box_title = request.form.get('title')
             tline = request.form.get('tline')
             slug = request.form.get('slug')
@@ -110,51 +118,49 @@ def edit(sno):
             img_file = request.form.get('img_file')
             date = datetime.now()
 
-            if sno == '0':  
+            if sno=='0':
                 post = Posts(title=box_title, slug=slug, content=content, tagline=tline, img_file=img_file, date=date)
                 db.session.add(post)
                 db.session.commit()
-                return redirect('/edit/' + str(post.sno)) 
-
-            else: 
+            else:
                 post = Posts.query.filter_by(sno=sno).first()
                 post.title = box_title
-                post.tagline = tline
                 post.slug = slug
                 post.content = content
+                post.tagline = tline
                 post.img_file = img_file
                 post.date = date
                 db.session.commit()
+                return redirect('/edit/'+sno)
+        post = Posts.query.filter_by(sno=sno).first()
+        return render_template('edit.html', params=params, post=post, sno=sno)
 
-        if sno != '0':  
-            post = Posts.query.filter_by(sno=sno).first()
-            return render_template('edit.html', params=params, post=post, sno=sno)
 
-        
-        return render_template('edit.html', params=params, sno=sno)
-
-    return redirect('/login') 
-
-@app.route("/contact", methods = ['GET', 'POST'])
+@app.route("/uploader", methods = ['GET', 'POST'])
 def uploader():
-    if "user" in session and session['user'] == params['admin_user']:
-        if request.method == "POST":
-            r = request.files['file1']
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-            return "Uploaded Succesfully..."
+    if ('user' in session and session['user'] == params['admin_user']):
+        if (request.method == 'POST'):
+            f= request.files['file1']
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename) ))
+            return "Uploaded successfully"
 
-@app.route('/logout')
+
+
+@app.route("/logout")
 def logout():
-    session.pop('user')
-    return redirect('/dashboard')
+    session.pop('user' ,None)  
+    flash("You have been logged out successfully.", "success")
+    return redirect("/")
 
-@app.route("/delete/<string:sno>" , methods=['GET', 'POST'])
+@app.route("/delete/<string:sno>", methods = ['GET', 'POST'])
 def delete(sno):
-    if "user" in session and session['user']==params['admin_user']:
+    if ('user' in session and session['user'] == params['admin_user']):
         post = Posts.query.filter_by(sno=sno).first()
         db.session.delete(post)
         db.session.commit()
-    return redirect("/dashboard")
+    return redirect('/dashboard')
+
+
 
 @app.route("/contact", methods = ['GET', 'POST'])
 def contact():
@@ -163,7 +169,7 @@ def contact():
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
-        entry = Contacts(name=name, phone_num = phone, message = message, date= datetime.now(),email = email )
+        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
         db.session.add(entry)
         db.session.commit()
         mail.send_message('New message from ' + name,
@@ -173,6 +179,8 @@ def contact():
                           )
     return render_template('contact.html', params=params)
 
+@app.context_processor
+def inject_common_data():
+    return dict(params=params)
 
 app.run(debug=True)
-
